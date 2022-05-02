560dc9c8ae40384b5b4a426e80b151f231bef2e9
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2022-05-02 22:03:49 +0700

    Fix main.cpp (i forgor 💀)

--- main/main.cpp
+++ main/main.cpp
@@ -3,6 +3,8 @@
 #include "SinhVien.h"
 #include "TabLTC.h"
 #include "TabMonHoc.h"
+#include "TabThongKeDiem.h"
+#include "TabThongKeSV.h"
 #include "drawing.h"
 #include "import.h"
 
@@ -15,7 +17,7 @@ int main() {
     // declare data structures and load data
     // TODO: also do the same for everything else here, or in switch cases
     DsMonHoc dsmh;
-    dsmh.read();
+    // dsmh.read();
 
     DSSV dssv;
     dssv.docFile();
@@ -24,14 +26,14 @@ int main() {
     dsltc.read();
 
     // start first tab: MonHoc
-    int key = initMHTab(dsmh);
+    int key = initMHTab(dsmh, dsltc);
 
     while (key != ESC) {
         // TODO: put other tab in here
         switch (key) {
             case KEY_F2:
                 drawSelectedTab(1);
-                initLTCTab(dsltc, dsmh, dssv);
+                initLTCTab(dsmh, dssv, dsltc);
                 break;
             case KEY_F3:
                 drawSelectedTab(2);
@@ -41,9 +43,11 @@ int main() {
                 break;
             case KEY_F5:
                 drawSelectedTab(4);
+                initThongKeSVTab(dssv);
                 break;
             case KEY_F6:
                 drawSelectedTab(5);
+                initThongKeDiemTab(dsmh, dssv, dsltc);
                 break;
             case KEY_F7:
                 drawSelectedTab(6);
@@ -51,11 +55,15 @@ int main() {
             case KEY_F8:
                 drawSelectedTab(7);
                 break;
+            case ESC:
+                drawSelectedTab(0);
+                initMHTab(dsmh, dsltc);
+                break;
         }
 
         // TODO: remove these 2 lines
         drawSelectedTab(0);
-        key = initMHTab(dsmh);
+        key = initMHTab(dsmh, dsltc);
     }
     SetColor();
     return 0;
